b18187c8a34f248a42c15d1a66a20bfb6cfae997
Mark A Guza <guzamaguza@gmail.com>
2020-01-14 05:24:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,9 +7,9 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: "
+      StringOut == "The line is currently: "
     array.each do |name|
-      puts "#{array.indexOf(name)}" + ". #{name}"
+      StringOut << "#{array.indexOf(name)}" + ". #{name}"
     end
   end
 end
