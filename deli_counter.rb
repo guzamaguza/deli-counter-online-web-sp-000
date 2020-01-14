69cffb03bc371ac4c64a03f9b0977a3524734a19
Mark A Guza <guzamaguza@gmail.com>
2020-01-14 05:22:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,11 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: " + "1."
+    puts "The line is currently: "
+    array.each do |name|
+      puts "#{array.indexOf(name)}" + "." + name 
+    end
+    "The line is currently: 1. Ada 2. Grace 3. Kent"
   end
 end
 
