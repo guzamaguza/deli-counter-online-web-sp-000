fb91dd295b56cbd7157f8524387a375b8f61f9c8
Mark A Guza <guzamaguza@gmail.com>
2020-01-14 05:17:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+
+
+katz_deli = []
+
+
+def line(array)
+  if array.length == 0 
+    puts "The line is currently empty."
+  else 
+    puts "The line is currently: " + "1." 
+  end
+end
+
+
+def take_a_number(katz_deli, name)
+
+
+end
+
+
+def now_serving
+    puts "There is nobody waiting to be served!"
+end
+
